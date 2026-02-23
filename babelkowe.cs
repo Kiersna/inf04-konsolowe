584610cbf844cf8a96379b5f9ad31b12122e3d9b
Kiersna <76652871+Kiersna@users.noreply.github.com>
2026-02-23 12:01:59 +0100

    Add files via upload

---
+++ babelkowe.cs
@@ -0,0 +1,26 @@
+﻿void babelkowe(int[] tablica)
+{
+    for (int i = 0; i < tablica.Length-1; i++)
+    {
+        for(int j = 0; j<tablica.Length-1-i; j++)
+        {
+            if (tablica[j] > tablica[j+1])
+            {
+                int temp = tablica[j]; 
+                tablica[j] = tablica[j+1];
+                tablica[j+1] = temp;
+            }
+        }
+    }
+    foreach(int i in tablica)
+    {
+        Console.Write(i+", ");
+    }
+}
+int[] tablica = new int[100];
+Random rnd = new Random();
+for(int i = 0; i <100; i++)
+{
+    tablica[i] = rnd.Next(0, 1000);
+}
+babelkowe(tablica);
